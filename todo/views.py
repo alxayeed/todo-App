d1f85b5ce8f6137f3eb5c760c468ed0e92490837
al <alxayeed@gmail.com>
2019-09-09 13:10:33 +0600

    todo completed works

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.decorators.http import require_POST
 
 from .models import Todo
 from .forms import TodoForm
@@ -10,3 +11,22 @@ def index(request):
 	context = {'todo_list' : todo_all,'form':form}
 
 	return render(request,'todo/index.html',context)
+
+@require_POST
+def addTodo(request):
+	form = TodoForm(request.POST)
+
+	print(request.POST['text'])
+	if form.is_valid():
+		new_todo = Todo(text=request.POST['text'])
+		new_todo.save()
+
+	return redirect('index')
+
+def todoComplete(request,todo_id):
+	todo = Todo.objects.get(pk=todo_id)
+	print(todo)
+	todo.completed = True
+	todo.save()
+
+	return redirect('index')
